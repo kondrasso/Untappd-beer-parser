8e57a090bf7935958ee1010bbc04db0d378b120b
kondrasso <steelsight@yandex.ru>
2020-06-14 07:07:37 +0300

    cleaned init py

--- source/beer.py
+++ source/beer.py
@@ -1,17 +1,3 @@
-import json
-import pandas as pd
-from time import sleep
-from tqdm.auto import tqdm
-from selenium import common
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as ec
-
-
 class ButtonPresser:
     """
     Class that contains method's to continuously press buttons
