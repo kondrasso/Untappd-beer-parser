71a8becebdc4c115de4f665a4d3094aa7e467b33
Ivan Kondratov <steelsight@yandex.ru>
2020-06-17 04:27:22 +0300

    Update example.py

--- example.py
+++ example.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     # initiate driver
     drv = DriverSetup(headless=False).selenium_driver
     # you should modify log_pass_dummy to be able to login on FB, or specify path to your own file
-    LoginProcess(drv, 'source', 'log_pass').log_in()
+    LoginProcess(drv, 'source', 'log_pass_dummy').log_in()
     # after logging in, we can collect info on menu of selected bars and save it to csv
     bars_menu = BarsMenu(drv, bars_to_get_menu, to_df=True, to_csv=True).parse_bars_menu()
 
